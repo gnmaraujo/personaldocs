fcc5c4da8798d222bc13ae264147652a30001e00
gnmaraujo <gnmaraujo@gmail.com>
2025-09-15 17:28:46 +0100

    ATUALIZANDO

---
+++ AtualizaConteudos.bat
@@ -0,0 +1,16 @@
+echo "# pers" >> README.md
+powershell -command "git init"
+powershell -command "git add README.md"
+
+powershell -command "git config --global user.email gnmaraujo@gmail.com"
+powershell -command "git config --global user.name gnmaraujo"
+powershell -command "git config --global user.password EntradaGH6600**"
+
+powershell -command "git add ."
+powershell -command "git commit -m 'ATUALIZANDO'"
+powershell -command "git push"
+
+
+echo ----------Atualizacao concluida----------------------------
+
+pause
