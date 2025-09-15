eaa663eafc590be03afe7e13299eb2098ad02816
gnmaraujo <gnmaraujo@gmail.com>
2025-09-15 17:35:18 +0100

    ATUALIZANDO

--- AtualizaConteudos.bat
+++ AtualizaConteudos.bat
@@ -8,7 +8,7 @@ powershell -command "git config --global user.password EntradaGH6600**"
 
 powershell -command "git add ."
 powershell -command "git commit -m 'ATUALIZANDO'"
-powershell -command "git push https://github.com/gnmaraujo/personaldocs.git"
+powershell -command "git push --set-upstream https://github.com/gnmaraujo/personaldocs.git master"
 
 
 echo ----------Atualizacao concluida----------------------------
